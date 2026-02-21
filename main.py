725cf8602dfb89ccfd4ba2f5c330913d8b977059
Prakash Verma <prkshverma09@gmail.com>
2026-02-21 16:54:22 +0000

    blah

---
+++ main.py
@@ -0,0 +1,12 @@
+from math_utils import add, divide
+
+def main():
+    print "Starting main application..."
+    res = add(10, 5)
+    print "Result of add: ", res
+
+    res = divide(10, 3)
+    print "Result of divide: ", res
+
+if __name__ == "__main__":
+    main()
---
+++ math_utils.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def divide(a, b):
+    # Legacy Python 2 style division check
+    print "Dividing ", a, " by ", b
+    return a / b
+
+def multiply(a, b):
+    return a * b
