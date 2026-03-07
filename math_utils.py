38392864f15f5a6c9acae5a0970f6d42c2559bb2
paverma <paverma@hubspot.com>
2026-03-07 22:48:03 +0000

    Migration: math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -3,7 +3,7 @@ def add(a, b):
 
 def divide(a, b):
     # Legacy Python 2 style division check
-    print "Dividing ", a, " by ", b
+    print("Dividing ", a, " by ", b)
     return a / b
 
 def multiply(a, b):
